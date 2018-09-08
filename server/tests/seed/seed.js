74e3075aab9978f354c07fb851e91e3d9f5fc8f6
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-09-08 14:16:54 +0300

    add users seeding to test database

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,6 +1,36 @@
 const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
+const { User } = require('../../models/user');
 const { Todo } = require('../../models/todo');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [
+  {
+    _id: userOneId,
+    email: 'john@example.com',
+    password: 'userOnePass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({ _id: userOneId, access: 'auth' }, 'somesecret').toString()
+    }]
+  },
+  {
+    _id: userTwoId,
+    email: 'doe@example.com',
+    password: 'userTwoPass'
+  }
+];
+
+const populateUsers = (done) => {
+  User.deleteMany().then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
 
 const todos = [
   {
@@ -21,4 +51,4 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-module.exports = { todos, populateTodos };
+module.exports = { users, populateUsers, todos, populateTodos };
