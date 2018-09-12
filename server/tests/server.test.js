b28e0227234ea70e24b573354fda1a00124f69e1
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-09-12 19:42:30 +0300

    test POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -212,11 +212,11 @@ describe('POST /users', () => {
           return done(err);
         }
 
-        User.findOne({email}).then((user) => {
+        User.findOne({ email }).then((user) => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((err) => done(err));
       });
   });
 
@@ -240,5 +240,58 @@ describe('POST /users', () => {
       .send({ email, password })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    const id = users[1]._id;
+    const email = users[1].email;
+    const password = users[1].password;
+
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    const id = users[1]._id;
+    const email = users[1].email;
+    const password = users[1].password + '1';
+
+    request(app)
+      .post('/users/login')
+      .send({ email, password })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
   });
 });
